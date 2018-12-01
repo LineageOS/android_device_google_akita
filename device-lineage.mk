0ac3cbdf550e45569d210d6b3f316c96d1935152
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    akita: Declare EUICC permissions
    This is a squash of the following:
    
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Sat Dec 1 13:53:00 2018 -0800
    
        Declare EUICC permission
    
        Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Oct 18 00:06:22 2019 -0500
    
        Move eUICC permissions xml to /product
    
        All eUICC items were moved to /product in Q.
    
        Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Mon Apr 8 00:46:17 2024 +0300
    
        shusky: Enable EUICC MEP feature
    
        As seen on stock
    
        Change-Id: If7c834ba2bb972b2867939a1f47468a0f1ff6e69
    
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- device-lineage.mk
+++ device-lineage.mk
@@ -3,3 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
