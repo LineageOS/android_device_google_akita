732c894462d9165110778791dee48de65d995f2d
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-12 04:11:07 +0530

    akita: Build EuiccSupportPixelOverlay
    Change-Id: I46db4122b89798457338f6751982865571529ba6

--- device-lineage.mk
+++ device-lineage.mk
@@ -16,6 +16,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
