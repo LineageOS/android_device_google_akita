7111543052c1a6cd7a43fb29f9d753b7f4680001
Michael Bestas <mikeioannina@gmail.com>
2024-06-12 22:24:13 +0300

    Add aam HAL to compatibility matrix
    Change-Id: I778cd13c318afc6048fff4b317bb6974dcc0da24

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,5 @@
 # SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
+
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/aam_compatibility_matrix.xml
