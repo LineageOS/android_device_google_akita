cd6286954353618e0191a1e2e963648a839229e9
Michael Bestas <mikeioannina@gmail.com>
2024-06-12 22:03:10 +0300

    Initial aam policy
    Change-Id: I81cf99339193df3ed5341fb34e338363cd63fa24

--- sepolicy/akita-sepolicy.mk
+++ sepolicy/akita-sepolicy.mk
@@ -1,4 +1,5 @@
 # sepolicy exclusively for akita.
+BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/aam
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/tracking_denials
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/radio
