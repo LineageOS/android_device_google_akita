2caca0f29ece2aed98cb9303406c1ed4e1d3cae7
Michael Bestas <mkbestas@lineageos.org>
2024-09-04 06:03:06 +0300

    Update fingerprint/build description from AP2A.240905.003.A1
    Change-Id: I3f252315d5677317f7445ebf456103d3cb6bb2f0

--- lineage_akita.mk
+++ lineage_akita.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=akita \
-    PRIVATE_BUILD_DESC="akita-user 14 AP2A.240805.005.A1 12030693 release-keys"
+    PRIVATE_BUILD_DESC="akita-user 14 AP2A.240905.003.A1 12234140 release-keys"
 
-BUILD_FINGERPRINT := google/akita/akita:14/AP2A.240805.005.A1/12030693:user/release-keys
+BUILD_FINGERPRINT := google/akita/akita:14/AP2A.240905.003.A1/12234140:user/release-keys
 
 $(call inherit-product, vendor/google/akita/akita-vendor.mk)
