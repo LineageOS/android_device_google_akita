LineageOS/android_device_google_raviole

Add initial Lineage product makefiles
Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

AndroidProducts.mk
@@ -31,7 +31,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_whitefin_pkvm.mk \
     $(LOCAL_DIR)/aosp_whitefin_hwasan.mk \
     $(LOCAL_DIR)/aosp_whitefin_64.mk \
-    $(LOCAL_DIR)/factory_whitefin.mk
+    $(LOCAL_DIR)/factory_whitefin.mk \
+    $(LOCAL_DIR)/lineage_oriole.mk \
+    $(LOCAL_DIR)/lineage_raven.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_slider-userdebug \

LineageOS/android_device_google_akita

Co-authored-by: Michael Bestas <mkbestas@gmail.com>

AndroidProducts.mk
@@ -17,7 +17,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_akita.mk \
     $(LOCAL_DIR)/aosp_akita_fullmte.mk \
-    $(LOCAL_DIR)/factory_akita.mk
+    $(LOCAL_DIR)/factory_akita.mk \
+    $(LOCAL_DIR)/lineage_akita.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_akita-trunk_staging-userdebug