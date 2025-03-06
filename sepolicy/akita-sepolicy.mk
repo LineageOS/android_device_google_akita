93b12139c43038439a35582066820a9fb9953bd0
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- sepolicy/akita-sepolicy.mk
+++ sepolicy/akita-sepolicy.mk
@@ -3,3 +3,6 @@ BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/aam
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/tracking_denials
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/radio
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
