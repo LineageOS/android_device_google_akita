a90436fd0b2bab88ff7f92a6bff0f8e3100347f3
Michael Bestas <mkbestas@gmail.com>
2025-03-07 19:35:51 +0200

    Include missing GNSS sepolicy
    Change-Id: I0b99da9950d7e631767943cf40e8fd562b947e33

--- sepolicy/akita-sepolicy.mk
+++ sepolicy/akita-sepolicy.mk
@@ -4,5 +4,6 @@ BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/tracking_denials
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/radio
 
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gps/pixel/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
