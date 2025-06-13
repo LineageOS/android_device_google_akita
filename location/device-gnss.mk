0f3b7f4f8cb4cd1dca2f64747d948fff06e71365
Michael Bestas <mkbestas@gmail.com>
2025-06-13 15:01:53 +0300

    akita: Restore location FCM
    Change-Id: Icd71d5dee7fba63b5005f63a469e9fda44108769

--- location/device-gnss.mk
+++ location/device-gnss.mk
@@ -1,6 +1,9 @@
 # include common gnss binaries
 -include vendor/samsung_slsi/gps/s5300/gnss_release.mk
 
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
+    device/google/akita/location/device_framework_matrix_product.xml
+
 # include customized gps config files
 PRODUCT_COPY_FILES += \
     device/google/akita/location/ca.pem:vendor/etc/gnss/ca.pem
