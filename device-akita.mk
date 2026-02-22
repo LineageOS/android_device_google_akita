af5e83da4c96a9bb7bb44057d8e54dc2ae752064
Michael Bestas <mkbestas@lineageos.org>
2026-02-23 00:17:21 +0200

    akita: Convert Aperture overlay to RRO
    Change-Id: I5833e0db244baba0e883027f68bdc8b618a9e026

--- device-akita.mk
+++ device-akita.mk
@@ -40,6 +40,9 @@ PRODUCT_PACKAGES += \
     ShannonImsOverlayProductAkita \
     SystemUIGoogleOverlayVendorAkita
 
+PRODUCT_PACKAGES += \
+    ApertureOverlayAkita
+
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/vendor.prop
