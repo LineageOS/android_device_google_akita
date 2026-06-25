084262a36402a8bcd39fe69804ba961a7656b6d1
Michael Bestas <mkbestas@lineageos.org>
2026-06-25 03:15:40 +0300

    akita: Rename horribly named manifest.xml
    Use the same filename as muzel to unbreak the build.
    
    Change-Id: I84b276c52a545944fe8973235b0894117fffd6f5

--- akita/extract-files.py
+++ akita/extract-files.py
@@ -86,6 +86,10 @@ module = ExtractUtilsModule(
 
 
 def fix_vendor_file_list(file_list: FileList):
+    file_list.get_file('vendor/etc/vintf/manifest/manifest.xml').set_dst(
+        'vendor/etc/vintf/manifest/manifest_mapper_framework.xml'
+    )
+
     module_suffix_file_paths = [
         'vendor/lib64/com.google.edgetpu_app_service-V6-ndk.so',
         'vendor/lib64/com.google.edgetpu_vendor_service-V2-ndk.so',
